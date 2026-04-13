3bc269ea8ba5b1b69902a055be1dab977f05dd4a
Javier Huerta <javier.huerta.c@mail.pucv.cl>
2026-04-13 02:15:16 -0400

    creacion script de data

---
+++ tareas/tarea1/data/robot_data.py
@@ -0,0 +1,15 @@
+def cargar_experimentos():
+    experimentos = {
+        "exp1":  {"politica": "PPO",      "ambiente": "real",      "ruta": "simple",     "ISE": ,"IAE": , "ITSE": , "ITAE": , "tiempo_s": "None","pasos": "None","reward_medio": "None"}
+        "exp2":  {"politica": "PPO-Mask", "ambiente": "real",      "ruta": "simple",     "ISE": ,"IAE": , "ITSE": , "ITAE": , "tiempo_s": "None","pasos": "None","reward_medio": "None"}
+        "exp3":  {"politica": "PPO",      "ambiente": "simulated", "ruta": "simple",     "ISE": ,"IAE": , "ITSE": , "ITAE": , "tiempo_s": "None","pasos": "None","reward_medio": "None"}
+        "exp4":  {"politica": "PPO-Mask", "ambiente": "simulated", "ruta": "simple",     "ISE": ,"IAE": , "ITSE": , "ITAE": , "tiempo_s": "None","pasos": "None","reward_medio": "None"}
+        "exp5":  {"politica": "PPO",      "ambiente": "real",      "ruta": "square",     "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp6":  {"politica": "PPO-Mask", "ambiente": "real",      "ruta": "square",     "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp7":  {"politica": "PPO",      "ambiente": "simulated", "ruta": "square",     "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp8":  {"politica": "PPO-Mask", "ambiente": "simulated", "ruta": "square",     "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp9":  {"politica": "PPO",      "ambiente": "real",      "ruta": "triangular", "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp10": {"politica": "PPO-Mask", "ambiente": "real",      "ruta": "triangular", "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp11": {"politica": "PPO",      "ambiente": "simulated", "ruta": "triangular", "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+        "exp12": {"politica": "PPO-Mask", "ambiente": "simulated", "ruta": "triangular", "ISE": "None","IAE": "None", "ITSE": "None", "ITAE": "None", "tiempo_s": ,"pasos": ,"reward_medio": }
+    }
